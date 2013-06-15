26f04acde267f72bf835329d6aa8e1ddfda5d6f5
Javier Alvarez <javier@crowosft.com.ar>
2013-06-15 13:03:11 -0300

    Adding view to show items for a seller

---
+++ MyMercadolibre/ItemListViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemListViewController.h
+//  MyMercadolibre
+//
+//  Created by Javier Alvarez on 6/15/13.
+//  Copyright (c) 2013 Javier Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemListViewController : UITableViewController
+
+@end
